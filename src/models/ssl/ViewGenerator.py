eb369b2da3aa7b1d88d69cb3fb58694155ece82b
Nikodem <nikodem.swierkowski@gmail.com>
2025-06-07 23:51:59 +0200

    add barlow twins

--- src/models/ssl/ViewGenerator.py
+++ src/models/ssl/ViewGenerator.py
@@ -1,30 +1,51 @@
-from torchvision.transforms import v2
-import torch
-import random
 from typing import Tuple
+import torch
+from torchvision.transforms import v2
+import random
 
-
-class BYOLViewGenerator:
+class ViewGenerator:
     """
-    BYOLViewGenerator applies a series of transformations to the input images to create two different views,
-    which are then used for training the BYOL model.
-    Args:
-        device (str): Device to run the transformations on ('cpu' or 'cuda').
-        image_size (int): Size of the input images (default is 64).
-        masked_aug (float): Whether to apply masked augmentation, and how big.
+    Generates augumented view for ssl methods
+    
+    Attributes:
+    - mode: str = "byol",
+    - device: str = "cpu",
+    - image_size: int = 64,
+    - mask_size_ratio: float = 0.0,
     """
-
     def __init__(
-        self, device: str = "cpu", image_size: int = 64, mask_size_ratio: float = 0.0
+        self,
+        mode: str = "byol",
+        device: str = "cpu",
+        image_size: int = 64,
+        mask_size_ratio: float = 0.0,
     ):
         self.device = device
         self.mask_size_ratio = mask_size_ratio
-        self.transform = self._get_byol_augmentation(
-            image_size, gaussian_blur_prob=1.0
-        ).to(self.device)
-        self.transform_prim = self._get_byol_augmentation(
-            image_size, gaussian_blur_prob=0.1
-        ).to(self.device)
+
+        if mode == "byol":
+            self.transform = self._get_aug(image_size, blur_p=1.0, solarize_p=0.0)
+            self.transform_prim = self._get_aug(image_size, blur_p=0.1, solarize_p=0.0)
+        elif mode == "barlow" or mode == "simclr":
+            self.transform = self._get_aug(image_size, blur_p=0.5, solarize_p=0.2)
+            self.transform_prim = self._get_aug(image_size, blur_p=0.5, solarize_p=0.2)
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+
+        self.transform = self.transform.to(device)
+        self.transform_prim = self.transform_prim.to(device)
+
+    def _get_aug(self, image_size, blur_p=0.5, solarize_p=0.2):
+        return v2.Compose([
+            v2.ToImage(),
+            v2.RandomResizedCrop(image_size, scale=(0.7, 1.0)),
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
+            v2.RandomApply([v2.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0))], p=blur_p),
+            v2.RandomApply([v2.RandomSolarize(threshold=0.5)], p=solarize_p),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5] * 3, std=[0.5] * 3),
+        ])
 
     def _apply_random_mask(self, images: torch.Tensor) -> torch.Tensor:
         """
@@ -56,24 +77,6 @@ class BYOLViewGenerator:
             images[i, :, y1:y2, x1:x2] = 0.5
 
         return images
-
-    def _get_byol_augmentation(
-        self, image_size: int, gaussian_blur_prob: float
-    ) -> v2.Compose:
-        return v2.Compose(
-            [
-                v2.ToImage(),
-                v2.RandomResizedCrop(image_size, scale=(0.7, 1.0)),
-                v2.RandomHorizontalFlip(p=0.5),
-                v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
-                v2.RandomApply(
-                    [v2.GaussianBlur(kernel_size=5, sigma=(0.4, 2))],
-                    p=gaussian_blur_prob,
-                ),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            ]
-        )
 
     def __call__(self, x_batch: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         v = self.transform(x_batch)
